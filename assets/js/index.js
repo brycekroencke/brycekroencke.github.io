a80d2a349c62f1d704d91f21619768630ef27c10
Bryce Kroencke <brycekroencke@gmail.com>
2020-09-23 15:19:03 -0700

    changed pdf name and fixed lightbox display

---
+++ assets/js/index.js
@@ -0,0 +1,38 @@
+/*
+Copyright 2020 Adobe
+All Rights Reserved.
+NOTICE: Adobe permits you to use, modify, and distribute this file in
+accordance with the terms of the Adobe license agreement accompanying
+it. If you have received this file from a source other than Adobe,
+then your use, modification, or distribution of it requires the prior
+written permission of Adobe.
+*/
+
+/* Pass the embed mode option here */
+const viewerConfig = {
+    embedMode: "LIGHT_BOX"
+};
+
+function previewFile()
+{
+    /* Initialize the AdobeDC View object */
+    var adobeDCView = new AdobeDC.View({
+        /* Pass your registered client id */
+        clientId: "3bdd2334c3cd406f80134edaaf06e4ab"
+    });
+
+    /* Invoke the file preview API on Adobe DC View object */
+    adobeDCView.previewFile({
+        /* Pass information on how to access the file */
+        content: {
+            /* Location of file where it is hosted */
+            location: {
+                url: "https://brycekroencke.github.io/publications/IEEE_ACCESS_Traffic_Classification.pdf",
+            },
+        },
+        /* Pass meta data of file */
+        metaData: {
+            fileName: "IEEE_ACCESS_Traffic_Classification.pdf"
+        }
+    }, viewerConfig);
+};
